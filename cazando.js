ba00ee59eba59f928dd4931d3eeb2621c1693119
Sonicse2099 <joseluissonico@hotmail.com>
2026-04-15 18:21:34 -0500

    feat: estructura inicial del juego cazando (HTML, CSS, JS)

---
+++ cazando.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("miCanvas");
+var ctx = canvas.getContext("2d");
+
+var personaAncho = 40;
+var personaLargo = 40;
+
+var comidaAncho = 20;
+var comidaAlto = 20;
+
