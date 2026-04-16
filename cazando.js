2ffcd9a2aff8b7b79ded287d3e67b2d7af7e6466
Sonicse2099 <joseluissonico@hotmail.com>
2026-04-15 19:24:22 -0500

    Haciendo parte 2

--- cazando.js
+++ cazando.js
@@ -1,9 +1,62 @@
-var canvas = document.getElementById("miCanvas");
+var canvas = document.getElementById("areaJuego");
 var ctx = canvas.getContext("2d");
 
-var personaAncho = 40;
-var personaLargo = 40;
+var gatoAncho = 40;
+var gatoLargo = 40;
 
 var comidaAncho = 20;
 var comidaAlto = 20;
 
+var gatoX = 0;
+var gatoY = 0;
+
+var comidaX = 0;
+var comidaY = 0;
+
+var puntos = 0;
+var tiempo = 10;
+var intervalo = null;
+
+function obtenerAlazar(maximo){
+    return Math.floor(Math.random()*maximo)
+}
+
+function actualizarPuntos(){
+    document.getElementById("puntos").textContent = puntos;
+}
+
+function mostrarMensaje(texto) {
+    document.getElementById("mensaje").textContent = texto;
+}
+
+function dibujarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto); 
+}
+function graficarGato() {
+    graficarRectangulo(gatoX, gatoY, gatoAncho, gatoLargo, "white");
+}
+
+function graficarComida() {
+    graficarRectangulo(comidaX, comidaY, comidaAncho, comidaAlto, "gold");
+}
+
+function iniciarJuego(){
+    gatoX = (canvas.width / 2) - (gatoAncho / 2);
+    gatoY = (canvas.heigth / 2) - (gatoLargo / 2);
+
+    comidaX = (canvas.width / 2) - (comidaAncho / 2);
+    comidaY = (canvas.heigth / 2) - (comidaAlto / 2);
+
+    puntos = 0;
+    tiempo = 10;
+    actualizarPuntos();
+    actualizarTiempo();
+    mostrarMensaje("");
+
+    graficarGato();
+    graficarComida();
+
+    intervalo = setInterval(restarTiempo, 1000);
+
+}
